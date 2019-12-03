d066e209616e19fc95556da17bb5482483aeceff
72720 <727208201@qq.com>
2019-12-03 21:24:55 +0800

    添加成为车主页面

---
+++ src/components/line-item.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+      <div class="bjt">
+        <img class="t1" src="https://www.atzuche.com/static/media/banner_txt.27b00f5f.png">
+      </div>
+      <div class="bd">
+      <el-menu  class="el-menu-demo" mode="horizontal" >
+        <el-menu-item index="1">全部订单</el-menu-item>
+      </el-menu>
+      <!--表单-->
+
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%"
+
+      >
+        <el-table-column
+          prop="caname"
+          label="车名"
+          width="120"
+          >
+        </el-table-column>
+        <el-table-column
+          prop="cacard"
+          label="车牌号"
+          width="140">
+        </el-table-column>
+        <el-table-column
+          prop="xia"
+          label="下单时间">
+        </el-table-column><el-table-column
+          prop="qu"
+          label="取车时间">
+        </el-table-column><el-table-column
+          prop="yong"
+          label="使用时长">
+        </el-table-column><el-table-column
+          prop="money"
+          label="实际需交金额">
+        </el-table-column>
+      </el-table>
+
+      </div>
+      <div class="aa"></div>
+    </div>
+
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        tableData: []
+      }
+    },
+    mounted:{
+
+
+    }
+  }
+</script>
+
+<style scoped>
+  .bjt{
+    background-image: url("https://www.atzuche.com/static/media/signup-back.a22bc70d.jpg");
+    width: 1922px;
+    margin-left: -10px;
+    height: 450px;
+}
+  .t1{
+    margin-right: 500px;
+    margin-top: 200px;
+  }
+
+  .bd{
+    width: 1500px;
+    margin-left: 200px;
+  }
+  /*.aa{
+    background-image: url("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575354984614&di=b70db9c654b54593c89ea48cab5353ab&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Fback_pic%2F04%2F24%2F63%2F5558369edcf214a.jpg%2521%2Ffwfh%2F804x482%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue%2Fwatermark%2Ftext%2FOTDorr7orqE%3D%2Ffont%2Fsimkai%2Falign%2Fsoutheast%2Fopacity%2F20%2Fsize%2F50");
+    width: 1922px;
+    margin-left: -10px;
+    height: 500px;
+    float: left;
+  }*/
+
+</style>
